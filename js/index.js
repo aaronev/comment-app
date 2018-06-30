f4520fc5794c9cfc451b5adcb0c88df847af9901
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-29 19:26:26 -0700

    finishes up the psudo code for js and the plan

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 /*
+  the first thing I want to do is find a way connect the js 
+  to the html set your vars, consts, lets
+
+  second using event listener and since it is a form 
+  prevent the form from even taking action
+
+  third is the algorithm that allows the user to click post
+  and the post is created
   
+  once I figure how to do this I should be good with js
+  and move on to css
 */

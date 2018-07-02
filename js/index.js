89fb92a4e4ab4f1dbedd508d74e617d4b1dcf0c3
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-02 10:49:50 -0700

    completes project mvp

--- js/index.js
+++ js/index.js
@@ -24,12 +24,23 @@ document.getElementById('form').addEventListener('submit',
     var newDiv = document.createElement('div')
     var newH5 = document.createElement('h5')
     var newP = document.createElement('p')
+    var allPosts = document.getElementById('all-posts')
     newH5.textContent = subject.value
     newP.textContent = post.value
     newDiv.classList.add('comment')
     newDiv.classList.add('container')
     newDiv.appendChild(newH5)
     newDiv.appendChild(newP)
-    document.getElementById('all-posts').appendChild(newDiv)
+
+    //Option 1 to append child at bottom 
+    // allPosts.appendChild(newDiv)
+
+    //I might want to sort the comment out by latest post to be on top
+    //there I have to find a way to save all post through cookies
+    //The way for me to do that is first append an element that shows date and time
+    ///NVM I just googled how to append before first child
+
+    //Option 2 to append child at top
+    allPosts.insertBefore(newDiv, allPosts.firstChild)
   }
 )
